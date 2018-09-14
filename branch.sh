d2141cf0dd30dcbc743e0512d1259fefbf793ed1
Nitesh-Nandan <niteshnitp5686@gmail.com>
2018-09-14 17:37:43 +0530

    Update branch.sh

--- branch.sh
+++ branch.sh
@@ -1,20 +1,39 @@
 eipp_cloud="agent_eipp eipp_common eipp_correspondence eipp_model eipp_ui eipp_core_util eipp_gateway eipp_payment"
-branch_name="$1"
+operation="$1"
+branch_name="$2"
 
-if [ -z "$branch_name" ]
+if [ "$#" -ne 2 ]
 then
-    echo "Please enter branch_name"
-    echo "./branch.sh [ branch_name ]"
+    echo "Invalid Command"
+    echo "./branch.sh [sw/csw] [ branch_name ]"
     exit 0
 fi
 
-for project in $eipp_cloud
+
+if [ "$operation" == 'csw' ]
+then
+   for project in $eipp_cloud
     do
         echo "************* Creating Branch : $branch_name in $project **************"
-        cd $project
+        echo "cd $project"
         echo "checking out falcondev of $project"
-        git checkout falcondev
+        #git checkout falcondev
         echo "creating $branch_name and checkingout $branch_name."
-        git checkout -b "$branch_name"
-        cd ..
+        #git checkout -b "$branch_name"
+        #cd ..
     done
+
+elif [ "$operation" == 'sw' ]
+then
+    for project in $eipp_cloud
+     do
+        echo "************* Switching Branch : $branch_name in $project **************"
+        echo "cd $project"
+        echo "checking out $branch_name of $project"
+        echo "git checkout $branch_name"
+        #cd ..
+     done
+else
+    echo "Invalid operation"
+    echo "./branch.sh [sw/csw] [ branch_name ]"
+fi
